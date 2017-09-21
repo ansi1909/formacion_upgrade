522d0bf741c15fcb835012c13e8c98c3c2e2c5db
mugiwarayial <yasaacisaias@gmail.com>
2017-09-21 16:51:15 -0400

    Listado completo de roles

---
+++ web/js/comun.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	$('#dt').DataTable({
+        responsive: true,
+        pageLength:10,
+        sPaginationType: "full_numbers",
+        oLanguage: {
+        	"sProcessing":    "Procesando...",
+            "sLengthMenu":    "Mostrar _MENU_ registros",
+            "sZeroRecords":   "No se encontraron resultados",
+            "sEmptyTable":    "Ningún dato disponible en esta tabla",
+            "sInfo":          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_.",
+            "sInfoEmpty":     "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":   "",
+            "sSearch":        "Buscar:",
+            "sUrl":           "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            oPaginate: {
+                sFirst: "<<",
+                sPrevious: "<",
+                sNext: ">", 
+                sLast: ">>" 
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
+
+
+});
