2d58481541a8466c6766622907308892fb7ee5fd
amst1909 <ansi79@gmail.com>
2017-09-22 17:16:21 -0400

    Modificación de la vista del listado de empresa, creacion de la ruta para la creacion de empresas, creaccion de la accion para la creacion de empresas y creacion del archivo de idiomas para el idioma espanol

--- src/Link/BackendBundle/Controller/EmpresaController.php
+++ src/Link/BackendBundle/Controller/EmpresaController.php
@@ -11,11 +11,56 @@ use Doctrine\ORM\EntityRepository;
 
 class EmpresaController extends Controller
 {
-   public function indexAction()
+   public function indexAction($app_id)
     {
+
+      $session = new Session();
+      $f = $this->get('funciones');
+      
+      if (!$session->get('ini'))
+      {
+          return $this->redirectToRoute('_loginAdmin');
+        }
+      else {
+
+        $session->set('app_id', $app_id);
+        if (!$f->accesoRoles($session->get('usuario')['roles'], $session->get('app_id')))
+        {
+          return $this->redirectToRoute('_authException');
+        }
+      }
+        
       $r = $this->getDoctrine()->getRepository('LinkComunBundle:AdminEmpresa');
       $empresas = $r->findAll();
 
       return $this->render('LinkBackendBundle:Empresa:index.html.twig', array('empresas'=>$empresas));
+    }
+
+    public function registroAction($empresa_id, Request $request){
+
+      $session = new Session();
+      $f = $this->get('funciones');
+      
+      if (!$session->get('ini'))
+      {
+          return $this->redirectToRoute('_loginAdmin');
+        }
+      else {
+        if (!$f->accesoRoles($session->get('usuario')['roles'], $session->get('app_id')))
+        {
+          return $this->redirectToRoute('_authException');
+        }
+      }
+
+      $em = $this->getDoctrine()->getManager();
+
+      if ($empresa_id) 
+      {
+        $empresa = $em->getRepository('LinkComunBundle:AdminEmpresa')->find($empresa_id);
+      }
+      else {
+        $empresa = new AdminEmpresa();
+      } 
+
     }
 }
