f6b27f092164f22c53344f99b5cda6d8f1d5246a
rvirguez <ricardovirguez@gmail.com>
2017-10-26 11:26:32 -0400

    arreglo de diseno

--- web/js/admin/permisosRol.js
+++ web/js/admin/permisosRol.js
@@ -4,17 +4,6 @@ $(document).ready(function() {
 		clickAplicacion($(this));
 	});
 
-	/*$( ".aplicacion" ).each(function( index ) {
-		clickAplicacion($(this));
-	});*/
-
-	/*$('.subaplicacion').click(function(){
-		var cl = $(this).attr('class');
-		var c = cl.split('subaplicacion');
-		var aplicacion_id = $.trim(c[1]);
-		clickAplicacion($('#f'+aplicacion_id));
-	});*/
-
 	$('#save').click(function(){
 		$('#save').hide();
 	});
@@ -25,16 +14,17 @@ function clickAplicacion(cb){
 	var id = cb.attr('id');
 	var i = id.split('f');
 	var aplicacion_id = i[1];
+	
 	if (cb.is(':checked'))
 	{
-		$('.tr-subaplicacion'+aplicacion_id).show();
+		$('.tr-subaplicacion'+aplicacion_id).show(1000);
 		$( ".subaplicacion"+aplicacion_id ).each(function( index ) {
 			// Activamos las sub-aplicaciones
 			$(this).prop('checked', true);
 		});
 	}
 	else {
-		$('.tr-subaplicacion'+aplicacion_id).hide();
+		$('.tr-subaplicacion'+aplicacion_id).hide(1000);
 		$( ".subaplicacion"+aplicacion_id ).each(function( index ) {
 			// Desactivamos las sub-aplicaciones
 			$(this).prop('checked', false);
