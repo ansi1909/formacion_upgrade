41045420590664f2010f1cd74894c159f670181e
mugiwarayial <yasaacisaias@gmail.com>
2017-11-21 17:00:20 -0400

    comienzo del modulo de participantes

--- src/Link/BackendBundle/Controller/UsuarioController.php
+++ src/Link/BackendBundle/Controller/UsuarioController.php
@@ -440,4 +440,30 @@ class UsuarioController extends Controller
         
     }
 
+    public function participantesAction($app_id, Request $request)
+    {
+        $session = new Session();
+        $f = $this->get('funciones');
+        
+        if (!$session->get('ini'))
+        {
+            return $this->redirectToRoute('_loginAdmin');
+        }
+        else {
+
+            $session->set('app_id', $app_id);
+            if (!$f->accesoRoles($session->get('usuario')['roles'], $session->get('app_id')))
+            {
+                return $this->redirectToRoute('_authException');
+            }
+        }
+        $f->setRequest($session->get('sesion_id'));
+
+        $empresas = $this->getDoctrine()->getRepository('LinkComunBundle:AdminEmpresa')->findAll(); 
+
+        return $this->render('LinkBackendBundle:Usuario:participantes.html.twig', array('empresas' =>$empresas));
+    }
+
 }
+
+    
