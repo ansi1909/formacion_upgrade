b87744570e1d0a676b6f86b6ad9c66c60be33e4f
José Enrique Velásquez <josenriquev@gmail.com>
2017-11-27 20:14:32 -0400

    Desarrollo del formulario de página.

--- src/Link/BackendBundle/Controller/PaginaController.php
+++ src/Link/BackendBundle/Controller/PaginaController.php
@@ -7,6 +7,13 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Link\ComunBundle\Entity\CertiPagina;
+use Link\ComunBundle\Entity\CertiCategoria;
+use Link\ComunBundle\Entity\CertiEstatusCategoria;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class PaginaController extends Controller
 {
@@ -70,6 +77,104 @@ class PaginaController extends Controller
 
         return $this->render('LinkBackendBundle:Pagina:pagina.html.twig', array('pagina' => $pagina,
                                                                                 'subpaginas' => $subpaginas));
+
+    }
+
+    public function editAction($pagina_padre_id, $pagina_id, $categoria_id, $estatus_contenido_id, $cantidad, $total, Request $request){
+
+        $session = new Session();
+        $f = $this->get('funciones');
+      
+        if (!$session->get('ini'))
+        {
+            return $this->redirectToRoute('_loginAdmin');
+        }
+        else {
+            if (!$f->accesoRoles($session->get('usuario')['roles'], $session->get('app_id')))
+            {
+                return $this->redirectToRoute('_authException');
+            }
+        }
+        $f->setRequest($session->get('sesion_id'));
+
+        $em = $this->getDoctrine()->getManager();
+        
+        if ($pagina_id) 
+        {
+            $pagina = $em->getRepository('LinkComunBundle:CertiPagina')->find($pagina_id);
+        }
+        else {
+            $pagina = new CertiPagina();
+            $pagina->setFechaCreacion(new \DateTime('now'));
+        }
+
+        if ($pagina_padre_id)
+        {
+            $pagina_padre = $em->getRepository('LinkComunBundle:CertiPagina')->find($pagina_padre_id);
+            $pagina->setPagina($pagina_padre);
+        }
+
+        if ($categoria_id)
+        {
+            $categoria = $em->getRepository('LinkComunBundle:CertiCategoria')->find($categoria_id);
+            $pagina->setCategoria($categoria);
+        }
+
+        if ($estatus_contenido_id)
+        {
+            $estatus_contenido = $em->getRepository('LinkComunBundle:CertiEstatusContenido')->find($estatus_contenido_id);
+            $pagina->setEstatusContenido($estatus_contenido);
+        }
+
+        $usuario = $em->getRepository('LinkComunBundle:AdminUsuario')->find($session->get('usuario')['id']);
+        $pagina->setUsuario($usuario);
+        $pagina->setFechaModificacion(new \DateTime('now'));
+
+        $form = $this->createFormBuilder($pagina)
+            ->setAction($this->generateUrl('_editPagina', array('pagina_padre_id' => $pagina_padre_id,
+                                                                'pagina_id' => $pagina_id,
+                                                                'categoria_id' => $categoria_id,
+                                                                'estatus_contenido_id' => $estatus_contenido_id,
+                                                                'cantidad' => $cantidad,
+                                                                'total' => $total)))
+            ->setMethod('POST')
+            ->add('nombre', TextType::class, array('label' => $this->get('translator')->trans('Nombre')))
+            ->add('categoria', EntityType::class, array('class' => 'Link\\ComunBundle\\Entity\\CertiCategoria',
+                                                        'choice_label' => 'nombre',
+                                                        'expanded' => false,
+                                                        'label' => $this->get('translator')->trans('Categoría'),
+                                                        'placeholder' => ''))
+            ->add('descripcion', TextareaType::class, array('label' => $this->get('translator')->trans('Descripción')))
+            ->add('contenido', TextareaType::class, array('label' => $this->get('translator')->trans('Contenido')))
+            ->add('foto', HiddenType::class, array('label' => $this->get('translator')->trans('Foto de la página'),
+                                                   'required' => false))
+            ->add('pdf', TextType::class, array('label' => $this->get('translator')->trans('Material complementario'),
+                                                'required' => false))
+            ->add('estatusContenido', EntityType::class, array('class' => 'Link\\ComunBundle\\Entity\\CertiEstatusContenido',
+                                                               'choice_label' => 'nombre',
+                                                               'expanded' => false,
+                                                               'label' => $this->get('translator')->trans('Estatus'),
+                                                               'placeholder' => ''))
+            ->add('save', SubmitType::class, array('label' => $this->get('translator')->trans('Finalizar')))
+            ->getForm();
+       
+
+        if ($request->getMethod() == 'POST')
+        {
+            $em->persist($plan);
+            $em->flush();
+            return $this->redirectToRoute('_showPagina', array('pagina_padre_id' => $pagina_padre_id,
+                                                               'pagina_id' => $pagina->getId(),
+                                                               'categoria_id' => $categoria_id,
+                                                               'estatus_contenido_id' => $estatus_contenido_id,
+                                                               'cantidad' => $cantidad,
+                                                               'total' => $total));
+        }
+        
+        return $this->render('LinkBackendBundle:Pagina:edit.html.twig', array('form' => $form->createView(),
+                                                                              'pagina' => $pagina,
+                                                                              'cantidad' => $cantidad,
+                                                                              'total' => $total));
 
     }
 
--- web/js/admin/paginas.js
+++ web/js/admin/paginas.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 
 	$('.tree').jstree();
 
+	$('.delete').click(function(){
+		var pagina_id = $(this).attr('data');
+		sweetAlertDelete(pagina_id, 'CertiPagina');
+	});
+
 	$('.pepa').click(function(){
 		var pagina_id = $(this).attr('data');
 		console.log('pagina_id: '+pagina_id);
