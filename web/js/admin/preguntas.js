26ff5297c583ae52431ab5a7ad8488cdbbbb4ff1
José Enrique Velásquez <josenriquev@gmail.com>
2018-01-18 14:02:22 -0400

    Reordenamiento de preguntas

--- web/js/admin/preguntas.js
+++ web/js/admin/preguntas.js
@@ -7,4 +7,20 @@ $(document).ready(function() {
 		sweetAlertDelete(pregunta_id, 'CertiPregunta');
 	});
 
+	var table = $('#dt').DataTable( {
+		destroy: true,
+        rowReorder: true
+    } );
+
+    table.on( 'row-reorder', function ( e, diff, edit ) {
+        
+        for ( var i=0, ien=diff.length ; i<ien ; i++ ) {
+            var rowData = table.row( diff[i].node ).data();
+            // Id del registro está en la segunda columna
+        	id = rowData[1];
+            reordenar(id, 'CertiPregunta', diff[i].newData);
+        }
+ 
+    });
+
 });
