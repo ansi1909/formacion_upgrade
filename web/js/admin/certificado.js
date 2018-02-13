adc2be7a877001c28f1efcb621ddc7c304ca27c3
yusenia zambrano <yuseniazambrano@gmail.com>
2018-02-12 23:18:56 -0400

    cambios en certificados y constancias

--- web/js/admin/certificado.js
+++ web/js/admin/certificado.js
@@ -1,44 +1,68 @@
 $(document).ready(function() {
 
-	var root_site = $('#root_site').val();
+    var root_site = $('#root_site').val();
 
-	validarEntidad($('#tipo_certificado_id').val());
 
-	$('#tipo_certificado_id').click(function()
-	{
-		validarEntidad($(this).val());
-	});
+    $('#empresa_id').change(function()
+    {
+        $('#tipo_certificado_id').val('');
+        $('#entidad').val('');
+        $('.tipo_entidad').html('');
 
-    $('.iframe-btn').fancybox({	
-		'width'		: 900,
-		'height'	: 900,
-		'type'		: 'iframe',
+    });
+
+    $('#tipo_certificado_id').change(function()
+    {
+        var tipo_certificado_id = $(this).val();
+        buscarEntidad(tipo_certificado_id);
+    });
+
+    $('.iframe-btn').fancybox({ 
+        'width'     : 900,
+        'height'    : 900,
+        'type'      : 'iframe',
         'autoScale' : false,
-		'autoSize'	: false
+        'autoSize'  : false
     });
 
  });
 
-function validarEntidad(valor)
+
+function buscarEntidad(tipo_certificado_id)
 {
-	if(valor>1)
-	{
-		$('.entidad').show();
-	}else
-	{
-		$('.entidad').hide();
-		$('#entidad').attr("value","0");
-	}
+    var url_tipo_certificado = $('#url_tipo_certificado').val();
+    var empresa_id = $('#empresa_id').val();
+
+    if(empresa_id!="" )//&& tipo_certificado_id!="")
+    {
+        $.ajax({
+            type: "GET",
+            url: url_tipo_certificado,
+            dataType: "json",
+            data: { tipo_certificado_id: tipo_certificado_id, empresa_id: empresa_id },
+            success: function(data){
+
+                $('.tipo_entidad').html(data.html);
+            },
+            error: function(){
+                $('#alert-error').html($('#error_msg-edit').val());
+                $('#div-alert').show();
+            }
+        });
+    }else
+    {
+        alert('Tiene que indicar la empresa y el tipo de certificado');
+    }
 }
 
 function responsive_filemanager_callback(field_id)
 {
-	// Ruta en el campo de texto
-	var url=jQuery('#'+field_id).val();
-	var arr = url.split('uploads/');
-	var new_image = arr[arr.length-1];
-	$('#'+field_id).val(new_image);
-	
-	if(field_id=="imagen")
-		$('#figure').html('<img src="'+url+'" width="100%">');
+    // Ruta en el campo de texto
+    var url=jQuery('#'+field_id).val();
+    var arr = url.split('uploads/');
+    var new_image = arr[arr.length-1];
+    $('#'+field_id).val(new_image);
+    
+    if(field_id=="imagen")
+        $('#figure').html('<img src="'+url+'" width="100%">');
 }
