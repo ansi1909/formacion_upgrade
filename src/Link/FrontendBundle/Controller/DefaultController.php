9c29eabb35a0350004f1d5697d87ae18b7ed87b3
Jhonatan Ponce <ponceelrelajado@gmail.com>
2018-03-01 00:59:23 -0400

    Ajustes preliminares del dashboard

--- src/Link/FrontendBundle/Controller/DefaultController.php
+++ src/Link/FrontendBundle/Controller/DefaultController.php
@@ -15,8 +15,8 @@ class DefaultController extends Controller
     public function indexAction()
     {
 
-    	/*$em = $this->getDoctrine()->getManager();
-        $session = new Session();
+    	$em = $this->getDoctrine()->getManager();
+        /*$session = new Session();
         $f = $this->get('funciones');
 
         if (!$session->get('ini'))
@@ -28,35 +28,117 @@ class DefaultController extends Controller
       	if ($this->container->get('session')->isStarted())
       	{
         	
-        	$empresa_id = $session->get('empresa_id');
-        	$datos_usuario = $session->get('usuario');
-        	$nombre_usuario = $datos_usuario['nombre'];
-        	$apellido_usuario = $datos_usuario['apellido'];
-        	$correo_usuario = $datos_usuario['correo'];
-        	$foto_usuario = $datos_usuario['foto'];
-        	$roles_usuario = $datos_usuario['roles'];
+        	$datos = $session;
+			$empresa_obj = $this->getDoctrine()->getRepository('LinkComunBundle:AdminEmpresa')->find($session->get('empresa')['id']);
+			$bienvenida = $empresa_obj->getBienvenida();
 
+			$usuario_session = $session->get('usuario');
+			$usuario_id = $datos_usuario['nombre'];
+
+			$query_actividad = $em->createQuery('SELECT ar FROM LinkComunBundle:CertiPaginaLog ar 
+												 WHERE ar.usuario = :usuario_id
+												 ORDER BY u.id DESC')
+		                          ->setParameter('usuario_id', $session->get('usuario')['id'])
+		                          ->setMaxResults(3);
+            $actividad_reciente = $query_actividad->getResult();
+
+            if(count($actividad_reciente) > 1){
+				$reciente = 1;
+            }else{
+				$reciente = 0;
+            }
       	}
       	else {
       		return $this->redirectToRoute('_login');
       	}*/
+      	$yml = Yaml::parse(file_get_contents($this->get('kernel')->getRootDir().'/config/parametros.yml'));
+      	$temporal = array();
 
-      	/*	$usuario_id = 2;
+      	$usuario_id = 2;
       	$usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($usuario_id);
-      	$query = $em->createQuery('SELECT ru FROM LinkComunBundle:AdminRolUsuario ru WHERE ru.usuario = :usuario_id')
-		            ->setParameter('usuario_id', $usuario->getId());
-        $roles_usuario = $query->getResult();
+      	$paginaEmpresa = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->findAll();
 
-		$nombre = $usuario->getNombre();
-		$apellido = $usuario->getApellido();
-		$correo = $usuario->getCorreoPersonal();
-		$foto = $usuario->getFoto();
-		$roles = $roles_usuario;*/
+      	$datosPagina = array();
+      	$roles_bk = array();
+        $roles_bk[] = $yml['parameters']['rol']['participante'];
+        $roles_bk[] = $yml['parameters']['rol']['tutor'];
+        $roles_ok = 0;
+        $participante = false;
+        $tutor = false;
+
+        $query = $em->createQuery('SELECT ru FROM LinkComunBundle:AdminRolUsuario ru WHERE ru.usuario = :usuario_id')
+                    ->setParameter('usuario_id', $usuario->getId());
+        $roles_usuario_db = $query->getResult();
+        
+        foreach ($roles_usuario_db as $rol_usuario)
+        {
+            // Verifico si el rol está dentro de los roles de backend
+            if (in_array($rol_usuario->getRol()->getId(), $roles_bk))
+            {
+                $roles_ok = 1;
+            }
+            if ($rol_usuario->getRol()->getId() == $yml['parameters']['rol']['participante'])
+            {
+                $participante = true;
+            }
+            if ($rol_usuario->getRol()->getId() == $yml['parameters']['rol']['tutor'])
+            {
+                $tutor = true;
+            }
+        }
+
+      	$datosUsuario = array('id' => $usuario->getId(),
+                              'nombre' => $usuario->getNombre(),
+                              'apellido' => $usuario->getApellido(),
+                              'correo' => $usuario->getCorreoPersonal(),
+                              'foto' => $usuario->getFoto(),
+                              'participante' => $participante,
+                              'tutor' => $tutor);
+        
+        // Se setea los datos de la empresa
+        $datosEmpresa = array('id' => $usuario->getEmpresa()->getId(),
+                              'nombre' => $usuario->getEmpresa()->getNombre(),
+                              'chat' => $usuario->getEmpresa()->getChatActivo(),
+                              'webinar' => $usuario->getEmpresa()->getWebinar(),
+                              'platilla' => 'twig para el layout principal',
+                              'logo' => 'url del logo de la empresa');
+
+        foreach ($paginaEmpresa as $pagina)
+        {
+
+            $datosPagina[] = array('id' => $pagina->getId(),
+                                    'nombre' => $pagina->getNombre(),
+                                    'categoria' => $pagina->getCategoria(),
+                                    'foto' => $pagina->getFoto(),
+                                    'tiene_evaluacion' => true,
+                                    'sub_paginas' =>  ''
+                                    );
+		}
+
+		$bienvenida = $usuario->getEmpresa()->getBienvenida();
+
+		$query_actividad = $em->createQuery('SELECT ar FROM LinkComunBundle:CertiPaginaLog ar 
+											 WHERE ar.usuario = :usuario_id
+											 ORDER BY ar.id DESC')
+	                          ->setParameter('usuario_id', $usuario->getId())
+	                          ->setMaxResults(3);
+	    $actividad_reciente = $query_actividad->getResult();
+
+	    if(count($actividad_reciente) > 1){
+			$reciente = 1;
+        }else{
+			$reciente = 0;
+        }
+
+		$datos = array('usuario'=>$datosUsuario,
+                       'empresa'=>$datosEmpresa,
+                       'paginas'=>$datosPagina);
 
 
-
-
-        return $this->render('LinkFrontendBundle:Default:index.html.twig');
+        return $this->render('LinkFrontendBundle:Default:index.html.twig', array('sesion' => $datos,
+                                                                                 'bienvenida' => $bienvenida,
+                                                                                 'reciente' => $reciente,
+                                                                                 'actividad_reciente' => $actividad_reciente));
 
         $response->headers->setCookie(new Cookie('Peter', 'Griffina', time() + 36, '/'));
 
