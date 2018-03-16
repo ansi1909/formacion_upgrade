1a8623a49dcb3dfbd0fd4e9149b17e7e6c7efa13
Jhonatan Ponce <ponceelrelajado@gmail.com>
2018-03-16 01:32:47 -0400

    fusionando los módulos de progaramas en un solo controller y ajustes preliminares para la página de incio de programa

--- src/Link/FrontendBundle/Controller/ProgramaController.php
+++ src/Link/FrontendBundle/Controller/ProgramaController.php
@@ -10,10 +10,18 @@ use Symfony\Component\Yaml\Yaml;
 use Link\ComunBundle\Entity\AdminSesion;
 use Symfony\Component\HttpFoundation\Cookie;
 
-class MisProgramasController extends Controller
+class ProgramaController extends Controller
 {
 
-    public function indexAction()
+    public function programaAction($programa_id, Request $request)
+    {
+
+       $pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->find($programa_id);
+       return $this->render('LinkFrontendBundle:Programa:index.html.twig', array('pagina' => $pagina));
+
+    }
+
+    public function misProgramasAction()
     {
 
         $em = $this->getDoctrine()->getManager();
@@ -102,7 +110,6 @@ class MisProgramasController extends Controller
             }
 
             $programas_aprobados = array();
-            $programas_en_curso = array();
             
             // Convertimos los id de las paginas de la sesion en un nuevo array
             $paginas_ids = array();
@@ -124,134 +131,48 @@ class MisProgramasController extends Controller
                                                                 'pagina' => $paginas_ids));
             $prog_completados = $group_query_completados->getResult();
 
-            // Buscamos los programas en curso y disponibles en la sesion
-            $query_en_curso = $em->createQuery('SELECT ar FROM LinkComunBundle:CertiPaginaLog ar 
-                                                WHERE ar.usuario = :usuario_id
-                                                AND ar.estatusPagina != :completada
-                                                ORDER BY ar.id DESC')
-                                ->setParameters(array('usuario_id' => $session->get('usuario')['id'],
-                                                      'completada' => $yml['parameters']['estatus_pagina']['completada']));
-            $prog_en_curso = $query_en_curso->getResult();
+            if(count($prog_completados) >=  1){
+                $completado = 1;
+                foreach ($prog_completados as $pg) {
 
-            $group_query_sin_actividad = $em->createQuery('SELECT cp.id as paginaid, cp.descripcion as descripcion, cp.nombre as nombrepagina, cp.foto as foto
-                                                        FROM LinkComunBundle:CertiPagina cp
-                                                        WHERE cp.id IN (:pagina)
-                                                        AND NOT EXISTS (SELECT l FROM LinkComunBundle:CertiPaginaLog l 
-                                                                       WHERE l.usuario = :usuario
-                                                                       AND l.pagina = cp.id) 
-                                                        ORDER BY cp.id ASC')
-                                          ->setParameters(array('usuario' => $session->get('usuario')['id'],
-                                                                'pagina' => $paginas_ids));
-            $prog_sin_actividad = $group_query_sin_actividad->getResult();
-            
-            foreach ($prog_completados as $pg) {
+                    $datos_certi_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPaginaEmpresa')->findOneBy(array('empresa' => $session->get('empresa')['id'],
+                                                                                                                                     'pagina' => $pg['paginaid']));
 
-                $datos_certi_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPaginaEmpresa')->findOneBy(array('empresa' => $session->get('empresa')['id'],
-                                                                                                                                 'pagina' => $pg['paginaid']));
-
-                if($datos_certi_pagina->getAcceso()){
-                    // aprobado y con acceso de seguir viendo
-                    $continuar = 2;
-                }else{
-                    // aprobado y sin poder ver solo descargar notas y certificados
-                    $continuar = 3;
-                }
+                    if($datos_certi_pagina->getAcceso()){
+                        // aprobado y con acceso de seguir viendo
+                        $continuar = 2;
+                    }else{
+                        // aprobado y sin poder ver solo descargar notas y certificados
+                        $continuar = 3;
+                    }
+                        
+                    if($pg['foto']){
+                        $imagen = $pg['foto'];
+                    }else{
+                        $imagen = 'http://localhost/formacion2.0/web/front/assets/img/liderazgo.png';
+                    }
+                   
+                    $programas_aprobados[]= array('id'=>$pg['paginaid'],
+                                                    'nombre'=>$pg['nombrepagina'],
+                                                    'imagen'=>$imagen,
+                                                    'descripcion'=>$pg['descripcion'],
+                                                    'fecha_vencimiento'=>$f->timeAgo($datos_certi_pagina->getFechaVencimiento()->format("Y/m/d")),
+                                                    'continuar'=>$continuar);
                     
-                if($pg['foto']){
-                    $imagen = $pg['foto'];
-                }else{
-                    $imagen = 'http://localhost/formacion2.0/web/front/assets/img/liderazgo.png';
                 }
-               
-                $programas_aprobados[]= array('id'=>$pg['paginaid'],
-                                                'nombre'=>$pg['nombrepagina'],
-                                                'imagen'=>$imagen,
-                                                'descripcion'=>$pg['descripcion'],
-                                                'fecha_vencimiento'=>$f->timeAgo($datos_certi_pagina->getFechaVencimiento()->format("Y/m/d")),
-                                                'continuar'=>$continuar);
-                
+            }else{
+                $completado = 0;
             }
 
-            foreach ($prog_en_curso as $pg) {
-
-                $continuar = 0;
-                    
-                // buscamos la página
-                $datos_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->find($pg['paginaid']);
-
-                // buscamos los datos de la pagina contra empresa para obntener la fecha de vencimiento
-                $datos_certi_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPaginaEmpresa')->findOneBy(array('empresa' => $session->get('empresa')['id'],
-                                                                                                                          'pagina' => $pg['paginaid']));
-
-                if($pg['foto']){
-                    $imagen = $pg['foto'];
-                }else{
-                    $imagen = 'http://localhost/formacion2.0/web/front/assets/img/liderazgo.png';
-                }
-
-                // creamos variables para añadir al array
-                $titulo_padre = $pg['nombrepagina'];
-                $titulo_hijo = '';
-                $imagen = $imagen;
-                $categoria = $datos_pagina->getCategoria()->getNombre();
-                $porcentaje = $datos_pagina->getPorcentajeAvance() ;
-                $fecha_vencimiento = $f->timeAgo($datos_certi_pagina->getFechaVencimiento()->format("Y/m/d"));
-
-                $programas_en_curso[]= array('id'=>$pg['paginaid'],
-                                             'titulo_padre'=>$titulo_padre,
-                                             'titulo_hijo'=>$titulo_hijo,
-                                             'imagen'=>$imagen,
-                                             'categoria'=>$categoria,
-                                             'fecha_vencimiento'=>$fecha_vencimiento,
-                                             'continuar'=>$continuar,
-                                             'porcentaje'=>$porcentaje);
-                
-            }
-
-            foreach ($prog_sin_actividad as $pg) {
-
-                $continuar = 0;
-                    
-                // buscamos la página
-                $datos_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->find($pg['paginaid']);
-
-                // buscamos los datos de la pagina contra empresa para obntener la fecha de vencimiento
-                $datos_certi_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPaginaEmpresa')->findOneBy(array('empresa' => $session->get('empresa')['id'],
-                                                                                                                                 'pagina' => $pg['paginaid']));
-
-                if($pg['foto']){
-                    $imagen = $pg['foto'];
-                }else{
-                    $imagen = 'http://localhost/formacion2.0/web/front/assets/img/liderazgo.png';
-                }
-
-                // creamos variables para añadir al array
-                $titulo_padre = $pg['nombrepagina'];
-                $titulo_hijo = '';
-                $imagen = $imagen;
-                $categoria = $datos_pagina->getCategoria()->getNombre();
-                $porcentaje = 0;
-                $fecha_vencimiento = $f->timeAgo($datos_certi_pagina->getFechaVencimiento()->format("Y/m/d"));
-                
-                $programas_en_curso[]= array('id'=>$pg['paginaid'],
-                                             'titulo_padre'=>$titulo_padre,
-                                             'titulo_hijo'=>$titulo_hijo,
-                                             'imagen'=>$imagen,
-                                             'categoria'=>$categoria,
-                                             'fecha_vencimiento'=>$fecha_vencimiento,
-                                             'continuar'=>$continuar,
-                                             'porcentaje'=>$porcentaje);
-                
-            }
         }
         else {
             return $this->redirectToRoute('_login');
         }
 
-        return $this->render('LinkFrontendBundle:MisProgramas:index.html.twig', array('reciente' => $reciente,
-                                                                                      'actividad_reciente' => $actividad_reciente,
-                                                                                      'programas_aprobados' => $programas_aprobados,
-                                                                                      'programas_en_curso' => $programas_en_curso));
+        return $this->render('LinkFrontendBundle:Programa:misProgramas.html.twig', array('reciente' => $reciente,
+                                                                                         'actividad_reciente' => $actividad_reciente,
+                                                                                         'completado' => $completado,
+                                                                                         'programas_aprobados' => $programas_aprobados));
 
         $response->headers->setCookie(new Cookie('Peter', 'Griffina', time() + 36, '/'));
 
@@ -259,5 +180,112 @@ class MisProgramasController extends Controller
     }
 
     
+    public function programasAction()
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $f = $this->get('funciones');
+        $session = new Session();
+
+        if (!$session->get('iniFront'))
+        {
+            return $this->redirectToRoute('_authExceptionEmpresa', array('mensaje' => $this->get('translator')->trans('Lo sentimos. Sesión expirada.')));
+        }
+        $f->setRequest($session->get('sesion_id'));
+
+        if ($this->container->get('session')->isStarted())
+        {
+            $yml = Yaml::parse(file_get_contents($this->get('kernel')->getRootDir().'/config/parametros.yml'));
+            $datos = $session;
+
+            $programas_disponibles = array();
+            
+            // Convertimos los id de las paginas de la sesion en un nuevo array
+            $paginas_ids = array();
+            foreach ($session->get('paginas') as $pg) {
+                $paginas_ids[] = $pg['id'];
+            }
+
+            // Buscamos los grupos disponibles para el usuario por los programas disponibles en la sesión
+            $group_query = $em->createQuery('SELECT cg FROM LinkComunBundle:CertiGrupo cg 
+                                            JOIN LinkComunBundle:CertiGrupoPagina cgp
+                                            WHERE cg.empresa = :empresa
+                                            AND  cgp.grupo = cg.id
+                                            AND cgp.pagina IN (:pagina)
+                                            ORDER BY cg.id ASC')
+                            ->setParameters(array('empresa' => $session->get('empresa')['id'],
+                                                  'pagina' => $paginas_ids));
+            $grupos = $group_query->getResult();
+
+            // Buscamos datos especificos de los programas de la sesion obteniendo el grupo al que pertenece cada programa
+            $query_pages_by_group = $em->createQuery('SELECT cg.nombre as nombregrupo, cp.id as paginaid, cp.descripcion as descripcion, cp.nombre as nombrepagina, cp.foto as foto 
+                                     FROM LinkComunBundle:CertiGrupo cg,
+                                          LinkComunBundle:CertiGrupoPagina cgp,
+                                          LinkComunBundle:CertiPagina cp
+                                    WHERE cg.empresa = :empresa
+                                    AND  cgp.grupo = cg.id
+                                    AND cp.id IN (:pagina)
+                                    AND cgp.pagina = cp.id
+                                    ORDER BY cg.id ASC')
+                            ->setParameters(array('empresa' => $session->get('empresa')['id'],
+                                                  'pagina' => $paginas_ids));
+            $pages_by_group = $query_pages_by_group->getResult();
+            
+            foreach ($pages_by_group as $pg) {
+
+                // contruimos un array con los datos necesarios para el template y el grupo de cada programa
+                $pag_obj = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->find($pg['paginaid']);
+
+
+                $datos_certi_pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPaginaEmpresa')->findOneBy(array('empresa' => $session->get('empresa')['id'],
+                                                                                                                                 'pagina' => $pg['paginaid']));
+
+                $datos_log = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPaginaLog')->findOneBy(array('usuario' => $session->get('usuario')['id'],
+                                                                                                                    'pagina' => $pg['paginaid']));
+                if($datos_log){
+                    if($datos_log->getEstatusPagina()->getId() == $yml['parameters']['estatus_pagina']['completada']){
+                        if($datos_certi_pagina->getAcceso()){
+                            // aprobado y con acceso de seguir viendo
+                            $continuar = 2;
+                        }else{
+                            // aprobado y sin poder ver solo descargar notas y certificados
+                            $continuar = 3;
+                        }
+                    }else{
+                       // cursando actualemnete el progarama - continuar
+                       $continuar = 1; 
+                    }
+                    
+                }else{
+                    // si registros - iniciar
+                    $continuar = 0;
+                }
+                if($pg['foto']){
+                    $imagen = $pg['foto'];
+                }else{
+                    $imagen = 'http://localhost/formacion2.0/web/front/assets/img/liderazgo.png';
+                }
+               
+                $programas_disponibles[]= array('id'=>$pg['paginaid'],
+                                                'nombre'=>$pg['nombrepagina'],
+                                                'nombregrupo'=>$pg['nombregrupo'],
+                                                'imagen'=>$imagen,
+                                                'descripcion'=>$pag_obj->getDescripcion(),
+                                                'fecha_vencimiento'=>$f->timeAgo($datos_certi_pagina->getFechaVencimiento()->format("Y/m/d")),
+                                                'continuar'=>$continuar);
+                
+            }
+        }
+        else {
+            return $this->redirectToRoute('_login');
+        }
+
+        return $this->render('LinkFrontendBundle:Programa:programas.html.twig', array('grupos' => $grupos,
+                                                                                         'programas_disponibles' => $programas_disponibles));
+
+        $response->headers->setCookie(new Cookie('Peter', 'Griffina', time() + 36, '/'));
+
+        return $response;        
+    }
 
 }
