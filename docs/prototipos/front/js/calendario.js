fff04e804fd5feb296a27226ed3d242243a6eaf6
rvirguez <ricardovirguez@gmail.com>
2018-03-19 16:28:41 -0400

    agregando la vista de calendario y sus estilos

---
+++ docs/prototipos/front/js/calendario.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var date = new Date();
+    var yyyy = date.getFullYear().toString();
+    var mm = (date.getMonth()+1).toString().length == 1 ? "0"+(date.getMonth()+1).toString() : (date.getMonth()+1).toString();
+    var dd  = (date.getDate()).toString().length == 1 ? "0"+(date.getDate()).toString() : (date.getDate()).toString();
+
+    $('#calendar').fullCalendar({
+        header: {
+            language: 'es',
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,basicWeek,basicDay',
+
+        },
+        defaultDate: yyyy+"-"+mm+"-"+dd,
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        selectable: false,
+        selectHelper: true,
+        eventRender: function(event, element) {
+            element.bind('click', function() {
+                var eve_id = event.id;
+                var comienzo = $.fullCalendar.formatDate(event.start, 'DD-MM-YYYY hh:mm a');
+                var final = event.end ? $.fullCalendar.formatDate(event.end, 'DD-MM-YYYY hh:mm a') : '';
+                $('#Modal #p-titulo').html(event.title);
+                $('#Modal #p-fechainicio').html(comienzo);
+                $('#Modal #p-fechafin').html(final);
+                $('#Modal #p-descripcion').html(event.descripcion);
+                $('#Modal #p-lugar').html(event.lugar);
+                $('#Modal #p-nivel').html(event.nivel);
+                $('#Modal').modal('show');
+            });
+        },
+        events: "",
+        timeFormat: 'hh:mm a'
+    });
+});
