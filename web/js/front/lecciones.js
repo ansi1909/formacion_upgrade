957da052c959f5629dceac95d731c3321abcf7f0
José Enrique Velásquez <josenriquev@gmail.com>
2018-03-21 15:37:04 -0400

    Secuencia correcta de las lecciones tomando en cuenta todos los casos.

--- web/js/front/lecciones.js
+++ web/js/front/lecciones.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
 
 		$('#'+circle_nav.attr('id')).removeClass('circle-less-vista'); // Suprime el css vista al tab que se está presionando
 		circle_nav.addClass('circle-less-viendo'); // Agrega el css viendo al tab que se está presionando
+		circle_nav.removeClass('less-disabled'); // Remueve el css de disables al tab que se está presionando
 		$('#tab_activo').val(circle_nav.attr('id'));
 		$('.nav-less-container').scrollTo($('.circle-less-viendo'));
 
@@ -46,14 +47,44 @@ $(document).ready(function() {
 	}
 
 	$('.next_lesson').click(function(){
+		$(this).hide();
 		var str = $(this).attr('data');
 		var arr = str.split('-');
-		var pagina_id = arr[0];
-		var step = arr[1];
-		var last = arr[2];
+		var programa_id = arr[0];
+		var subpagina_id = arr[1];
+		var step = arr[2];
+		var last = arr[3];
 		if (last == 1)
 		{
 			// Es la última lección. Se determina qué lección falta por ver antes de finalizar.
+			var faltante = 0;
+			var pagina_faltante = 0;
+			$('.circle-nav').each(function(index){
+				var nav_a = $(this);
+				var class_str = nav_a.attr('class');
+				if ((class_str.indexOf('circle-less-vista') == -1) && (class_str.indexOf('circle-less-viendo') == -1))
+				{
+					if (faltante == 0)
+					{
+						pagina_faltante = nav_a.attr('data');
+					}
+					faltante = 1;
+				}
+			});
+			if (faltante == 1)
+			{
+				// Nos vamos al primer tab faltante
+				$('.btn-primary').show();
+				nextPage(pagina_faltante);
+			}
+			else {
+				// Se finaliza esta última lección y se redirige a la pantalla de final
+				var ok = finishLesson(programa_id, subpagina_id);
+				if (ok > 0)
+				{
+					window.location.replace($('#url_fin').val()+'/'+$('#puntos_agregados').val());
+				}
+			}
 		}
 		else {
 			// Continuar a la siguiente lección
@@ -62,6 +93,7 @@ $(document).ready(function() {
 			var id_str = $('.tab-'+step).attr('id');
 			if (id_str != 'one-tab')
 			{
+				$('.btn-primary').show();
 				id_arr = id_str.split('-');
 				nextPage(id_arr[1]);
 			}
@@ -70,35 +102,25 @@ $(document).ready(function() {
 
 	$('#next_subpage').click(function(){
 		
+		$(this).hide();
 		var str = $(this).attr('data');
 		var arr = str.split('-');
 		var prog_id = arr[0];
 		var pagina_id = arr[1];
 		var subpag_id = arr[2];
 
-		// Procesar página
-		$.ajax({
-			type: "POST",
-			url: $('#url_procesar').val(),
-			async: true,
-			data: { programa_id: prog_id, pagina_id: pagina_id },
-			dataType: "json",
-			success: function(data) {
-				console.log('Log Id: '+data.id);
-				// Redireccionar al siguiente URL
-				if (subpag_id > 0)
-				{
-					window.location.replace($('#url_next').val()+'/'+subpag_id);
-				}
-				else {
-					// Página de fin
-				}
-				//clearTimeout( timerId );
-			},
-			error: function(){
-				console.log('Error procesando la página'); // Hay que implementar los mensajes de error para el frontend
+		var ok = finishLesson(prog_id, pagina_id);
+
+		if (ok > 0)
+		{
+			if (subpag_id > 0)
+			{
+				window.location.replace($('#url_next').val()+'/'+subpag_id+'/'+$('#puntos_agregados').val());
 			}
-		});
+			else {
+				window.location.replace($('#url_fin').val()+'/'+$('#puntos_agregados').val());
+			}
+		}
 
 	});
 
@@ -111,9 +133,6 @@ function nextPage(pagina_id)
 	var nav_item = $( "#tab-"+pagina_id );
 	if (nav_item.length)
 	{
-		$('#tab_activo').val('tab-'+pagina_id);
-		$('#pagina_id_viendo').val(pagina_id);
-		nav_item.removeClass('less-disabled');
 		nav_item.trigger( "click" );
 	}
 	else {
@@ -147,11 +166,17 @@ function finishLesson(programa_id, pagina_id)
 	$.ajax({
 		type: "POST",
 		url: $('#url_procesar').val(),
-		async: true,
+		async: false,
 		data: { programa_id: programa_id, pagina_id: pagina_id },
 		dataType: "json",
 		success: function(data) {
-			console.log('Log procesado: '+data.id);
+			console.log('Log_puntos procesado: '+data.id);
+			// Se van sumando los puntos obtenidos en la lección
+			var str = data.id;
+			var log_puntos = str.split('_');
+			var puntos = $('#puntos_agregados').val();
+			puntos = parseInt(puntos) + parseInt(log_puntos[1]);
+			$('#puntos_agregados').val(puntos);
 			ok = 1;
 			//clearTimeout( timerId );
 		},
