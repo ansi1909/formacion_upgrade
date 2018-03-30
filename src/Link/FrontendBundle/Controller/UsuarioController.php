339e7559c625c1def43a60fca721076d0920cbed
mugiwarayial <yasaacisaias@gmail.com>
2018-03-30 15:12:17 -0400

    Preferencia de usuario, modificar datos y cambio de contra'esa.  paginador de biblioteca virtual y noticias y novedades

--- src/Link/FrontendBundle/Controller/UsuarioController.php
+++ src/Link/FrontendBundle/Controller/UsuarioController.php
@@ -34,10 +34,21 @@ class UsuarioController extends Controller
             if ($request->getMethod() == 'POST')
             {
                 $user = $request->request->get('username');
+                $mailp = $request->request->get('mailp');
+                $mailad = $request->request->get('mailad');
+                $fechan = $request->request->get('fechan');
                 
                 $usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($usuario_id);
                 
                 $usuario->setLogin($user);
+                $usuario->setCorreoPersonal($mailp);
+                $usuario->setCorreoCorporativo($mailad);
+                $fn_array = explode("-", $fechan);
+                $d = $fn_array[0];
+                $m = $fn_array[1];
+                $a = $fn_array[2];
+                $fechan = "$a-$m-$d";
+                $usuario->setFechaNacimiento(new \DateTime($fechan));
 
                 $em->persist($usuario);
                 $em->flush();   
@@ -51,11 +62,41 @@ class UsuarioController extends Controller
             return $this->redirectToRoute('_login');
         }
 
-        return $this->render('LinkFrontendBundle:Usuario:index.html.twig', array('usuario' => $usuario));
+        return $this->render('LinkFrontendBundle:Usuario:index.html.twig', array('usuario' => $usuario,
+                                                                                 'fecha' => $usuario->getFechaNacimiento()->format('Y-m-d')));
 
         $response->headers->setCookie(new Cookie('Peter', 'Griffina', time() + 36, '/'));
 
         return $response;
 
+    }
+
+    public function ajaxVerificarAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $f = $this->get('funciones');
+
+        $html ='';
+        $usuario_id = $request->request->get('usuario_id');
+        $nueva = $request->request->get('p_new');
+        $confirmar = $request->request->get('p_conf');
+
+        if ($nueva == $confirmar) {
+
+            $usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($usuario_id); 
+
+            $usuario->setClave($nueva);
+            $em->persist($usuario);
+            $em->flush();
+
+            $html ='<strong>Contraseña cambiada con exito</strong>';
+        }else
+        {
+            $html='<strong>Contraseñas no coinciden</strong>';
+        }
+
+        $return = array('html' => $html);
+        $return = json_encode($return);
+        return new Response($return, 200, array('Content-Type' => 'application/json'));
     }
 }
