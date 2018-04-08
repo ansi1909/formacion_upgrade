20bf6a771111ec8e526427338de8f5d18bae0906
rvirguez <ricardovirguez@gmail.com>
2018-04-08 19:32:12 -0400

    agregando detalles menores

--- docs/prototipos/front/js/custom.js
+++ docs/prototipos/front/js/custom.js
@@ -41,113 +41,112 @@ $(document).ready(function() {
   // do stuff when DOM is ready
 // $("#message").fadeIn("slow");
     var x = 0, y = 0, z = 0; 
-$("#config").on('click',function(){
-    if(x == 0){
-        if(y == 1){
-            $(".dropDownNotify").slideUp(50, function(){
-                $(".markNotify").fadeOut(100);
+    $("#config").on('click',function(){
+        if(x == 0){
+            if(y == 1){
+                $(".dropDownNotify").slideUp(50, function(){
+                    $(".markNotify").fadeOut(100);
+                });
+                y = 0;
+            }else if(z == 1){
+                $(".dropDownApps").slideUp(50, function(){
+                    $(".markApps").fadeOut(100);
+                });
+                z = 0;
+            }
+            $(".dropDownMenu").slideDown(500, function(){
+                $(".mark").fadeIn(100).delay(25);
+                $(".opcList").show();
+            }).delay(100);
+            x = 1;
+        }else{
+            $(".dropDownMenu").slideUp(500, function(){
+                $(".mark").fadeOut(600).delay(50);
             });
-            y = 0;
-        }else if(z == 1){
-            $(".dropDownApps").slideUp(50, function(){
-                $(".markApps").fadeOut(100);
-            });
-            z = 0;
-        }
-        $(".dropDownMenu").slideDown(500, function(){
-            $(".mark").fadeIn(100).delay(25);
-            $(".opcList").show();
-        }).delay(100);
-        x = 1;
-    }else{
-        $(".dropDownMenu").slideUp(500, function(){
-            $(".mark").fadeOut(600).delay(50);
-        });
-        x = 0;
-    }
-});
-
-$("#notify").on('click',function(){
-    if(y == 0){
-        if(x == 1){
-            $(".dropDownMenu").slideUp(50);
             x = 0;
-        }else if(z == 1){
-            $(".dropDownApps").slideUp(50, function(){
-                $(".markApps").fadeOut(100);
-            });
-            z = 0;
         }
-        $(".dropDownNotify").slideDown(500, function(){
-            $(".markNotify").fadeIn(100).delay(25);
-            $(".opcListNotify").show();
-        });
-        y = 1;
-    }else {
-        $(".dropDownNotify").slideUp(500, function(){
-            $(".markNotify").fadeOut(600);
-        });
-        y = 0;
-    }
-});
+    });
 
-$("#apps").on('click',function(){
-    if(z == 0){
-        if(x == 1){
-            $(".dropDownMenu").slideUp(50);
-            x = 0;
-        }else if(y == 1){
-            $(".dropDownNotify").slideUp(50, function(){
-                $(".markNotify").fadeOut(100);
+    $("#notify").on('click',function(){
+        if(y == 0){
+            if(x == 1){
+                $(".dropDownMenu").slideUp(50);
+                x = 0;
+            }else if(z == 1){
+                $(".dropDownApps").slideUp(50, function(){
+                    $(".markApps").fadeOut(100);
+                });
+                z = 0;
+            }
+            $(".dropDownNotify").slideDown(500, function(){
+                $(".markNotify").fadeIn(100).delay(25);
+                $(".opcListNotify").show();
+            });
+            y = 1;
+        }else {
+            $(".dropDownNotify").slideUp(500, function(){
+                $(".markNotify").fadeOut(600);
             });
             y = 0;
         }
-        $(".dropDownApps").slideDown(500, function(){
-            $(".markApps").fadeIn(100).delay(25);
-            $(".opcApps").show();
-        });
-        z = 1;
-    }else {
-        $(".dropDownApps").slideUp(500, function(){
-            $(".markApps").fadeOut(600);
-        });
-        z = 0;
-    }
-});
+    });
+
+    $("#apps").on('click',function(){
+        if(z == 0){
+            if(x == 1){
+                $(".dropDownMenu").slideUp(50);
+                x = 0;
+            }else if(y == 1){
+                $(".dropDownNotify").slideUp(50, function(){
+                    $(".markNotify").fadeOut(100);
+                });
+                y = 0;
+            }
+            $(".dropDownApps").slideDown(500, function(){
+                $(".markApps").fadeIn(100).delay(25);
+                $(".opcApps").show();
+            });
+            z = 1;
+        }else {
+            $(".dropDownApps").slideUp(500, function(){
+                $(".markApps").fadeOut(600);
+            });
+            z = 0;
+        }
+    });
     
-$("#main, #mainSidenav").on('click', function(){
-   if(x == 1){
-       $(".dropDownMenu").slideUp(500);
-       x = 0;
-   }else if(y == 1){
-       $(".dropDownNotify").slideUp(500, function(){
-           $(".markNotify").fadeOut(300).delay(50);
-       });
-       y = 0;
-   }else if(z == 1){
-       $(".dropDownApps").slideUp(500, function(){
-           $(".markApps").fadeOut(300).delay(50);
-       });
-       z = 0;
-   }
-});   
+    $("#main, #mainSidenav").on('click', function(){
+       if(x == 1){
+           $(".dropDownMenu").slideUp(500);
+           x = 0;
+       }else if(y == 1){
+           $(".dropDownNotify").slideUp(500, function(){
+               $(".markNotify").fadeOut(300).delay(50);
+           });
+           y = 0;
+       }else if(z == 1){
+           $(".dropDownApps").slideUp(500, function(){
+               $(".markApps").fadeOut(300).delay(50);
+           });
+           z = 0;
+       }
+    });   
     
-$("#equis").click(function(){
+    $("#equis").click(function(){
         $("#message").fadeOut("slow");
     });
 
-$("#arrowDown").click(function(){
+    $("#arrowDown").click(function(){
         $(".well-message-prog p").toggleClass("up");
         $("#arrowDown").toggleClass("arrow-move");
 
     });
 
-$("#iconComments").click(function(){
-            $("#comments").toggleClass("open-comments");
-            $("#main").toggleClass("ml-comments");
-            if (!$(".main-sidenav-close")[0]){
-                 closeNav();
-            }
-        });
-
+    $("#iconComments").click(function(){
+        $("#comments").toggleClass("open-comments");
+        $("#main").toggleClass("ml-comments");
+        if (!$(".main-sidenav-close")[0]){
+             closeNav();
+        }
+    });
 });
