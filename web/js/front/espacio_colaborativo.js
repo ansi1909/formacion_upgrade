d9e0de3d08598e53d04c9b737ee900fca1904545
José Enrique Velásquez <josenriquev@gmail.com>
2018-04-23 19:48:15 -0400

    Validaciones del formulario de nuevo espacio colaborativo

--- web/js/front/espacio_colaborativo.js
+++ web/js/front/espacio_colaborativo.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
-	
+	$('#fechaPublicacion').datepicker({
+	    startView: 1,
+	    autoclose: true,
+	    format: 'dd/mm/yyyy',
+	    language: 'es',
+	    startDate: '0d',
+	    clearBtn: true
+	})
+	.on( "changeDate", function(selected) {
+		var startDate = new Date(selected.date.valueOf());
+    	$('#fechaVencimiento').datepicker('setStartDate', startDate);
+    });
+
+	$('#fechaVencimiento').datepicker({
+	    startView: 1,
+	    autoclose: true,
+	    format: 'dd/mm/yyyy',
+	    language: 'es',
+	    startDate: '0d',
+	    clearBtn: true
+	})
+	.on( "changeDate", function(selected) {
+		var endDate = new Date(selected.date.valueOf());
+    	$('#fechaPublicacion').datepicker('setEndDate', endDate);
+	});
 
 });
