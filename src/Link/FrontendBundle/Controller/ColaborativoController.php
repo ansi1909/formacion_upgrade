0482c9e6dff1d1d6330dfc70a019cf6960399b93
Jose Velasquez <josenriquev@gmail.com>
2018-04-24 17:09:02 -0400

    New, Edit and Save de espacio colaborativo.

--- src/Link/FrontendBundle/Controller/ColaborativoController.php
+++ src/Link/FrontendBundle/Controller/ColaborativoController.php
@@ -40,12 +40,12 @@ class ColaborativoController extends Controller
         foreach ($foros_bd as $foro)
         {
             $listar = 0;
-            if ($foro->getFechaPublicacion()->format('Y-m-d') >= date('Y-m-d') || $foro->getFechaVencimiento()->format('Y-m-d') <= date('Y-m-d'))
+            if ($foro->getFechaPublicacion()->format('Y-m-d') > date('Y-m-d') || $foro->getFechaVencimiento()->format('Y-m-d') < date('Y-m-d'))
             {
                 if ($session->get('usuario')['tutor'])
                 {
                     $listar = 1;
-                    if ($foro->getFechaPublicacion()->format('Y-m-d') >= date('Y-m-d'))
+                    if ($foro->getFechaPublicacion()->format('Y-m-d') > date('Y-m-d'))
                     {
                         $name_ft = $this->get('translator')->trans('Aún sin publicar');
                         $coment_f = $this->get('translator')->trans('Fecha de Publicación');
@@ -89,7 +89,8 @@ class ColaborativoController extends Controller
                                  'name_ft' => $name_ft,
                                  'coment_f' => $coment_f,
                                  'coment_f_span' => $coment_f_span,
-                                 'resp_ft' => $resp_ft);
+                                 'resp_ft' => $resp_ft,
+                                 'usuario_id' => $foro->getUsuario()->getId());
             }
         }
 
@@ -120,6 +121,140 @@ class ColaborativoController extends Controller
                                                                                       'subpagina_id' => $subpagina_id,
                                                                                       'menu_str' => $menu_str,));
 
+    }
+
+    public function ajaxSaveForoAction(Request $request)
+    {
+        
+        $session = new Session();
+        $em = $this->getDoctrine()->getManager();
+        $html = '';
+
+        $foro_id = $request->request->get('foro_id');
+        $pagina_id = $request->request->get('pagina_id');
+        $subpagina_id = $request->request->get('subpagina_id');
+        $tema = $request->request->get('tema');
+        $fechaPublicacion = $request->request->get('fechaPublicacion');
+        $fechaVencimiento = $request->request->get('fechaVencimiento');
+        $mensaje = $request->request->get('mensaje_content');
+
+        if (!$foro_id)
+        {
+
+            $pagina = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->find($pagina_id);
+            $empresa = $this->getDoctrine()->getRepository('LinkComunBundle:AdminEmpresa')->find($session->get('empresa')['id']);
+            $usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($session->get('usuario')['id']);
+
+            $foro = new CertiForo();
+            $foro->setFechaRegistro(new \DateTime('now'));
+            $foro->setPagina($pagina);
+            $foro->setEmpresa($empresa);
+            $foro->setPagina($pagina);
+            $foro->setUsuario($usuario);
+
+        }
+        else {
+            $foro = $this->getDoctrine()->getRepository('LinkComunBundle:CertiForo')->find($foro_id);
+        }
+
+        $foro->setTema($tema);
+        $foro->setMensaje($mensaje);
+
+        list($d, $m, $a) = explode("/", $fechaPublicacion);
+        $fechaPublicacion = "$a-$m-$d";
+        $foro->setFechaPublicacion(new \DateTime($fechaPublicacion));
+
+        list($d, $m, $a) = explode("/", $fechaVencimiento);
+        $fechaVencimiento = "$a-$m-$d";
+        $foro->setFechaVencimiento(new \DateTime($fechaVencimiento));
+
+        $em->persist($foro);
+        $em->flush();
+
+        $foro_hijo = $em->getRepository('LinkComunBundle:CertiForo')->findOneBy(array('foro' => $foro->getId()),
+                                                                                array('fechaRegistro' => 'DESC'));
+
+        if (!$foro_hijo)
+        {
+            $name_ft = $foro->getUsuario()->getNombre().' '.$foro->getUsuario()->getApellido();
+            $coment_f = $this->get('translator')->trans('Fecha de Publicación');
+            $coment_f_span = $foro->getFechaPublicacion()->format('d/m/Y');
+            $resp_ft = 0;
+        }
+        else {
+            $query = $em->createQuery('SELECT COUNT(f.id) FROM LinkComunBundle:CertiForo f 
+                                        WHERE f.foro = :foro_id')
+                        ->setParameter('foro_id', $foro->getId());
+            $total_comentarios = $query->getSingleScalarResult();
+            $name_ft = $foro_hijo->getUsuario()->getNombre().' '.$foro_hijo->getUsuario()->getApellido();
+            $coment_f = $this->get('translator')->trans('Hizo un comentario');
+            $coment_f_span = $f->sinceTime($foro_hijo->getFechaPublicacion());
+            $resp_ft = $total_comentarios;
+        }
+
+        if (!$foro_id)
+        {
+            $html .= '<li class="f-table" id="liForo-'.$foro->getId().'">';
+        }
+
+        $class_tutor = $session->get('usuario')['tutor'] ? 'activar-destacado' : '';
+        $html .= '<div class="row justify-content-between">
+                    <div class="col-6">
+                        <a href="'.$this->generateUrl('_detalleColaborativo', array('foro_id' => $foro->getId(), 'subpagina_id' => $subpagina_id)).'"><h5 class="titulo_f-table">'.$foro->getTema().'</h5></a>
+                    </div>
+                    <div class="col-2">
+                        <div class="status_f-table '.$class_tutor.'">
+                            <a href="#" class="newTopic" data-toggle="modal" data-target="#modalnew" data="'.$foro->getId().'"><span class="status_ft">'.$this->get('translator')->trans('Editar').'</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-end foo-esp_col">
+                    <div class="col-auto">
+                        <span class="name_ft">'.$foro->getUsuario()->getNombre().' '.$foro->getUsuario()->getApellido().'</span>
+                    </div>
+                    <div class="col-auto">
+                        <span class="coment_f-table">'.$this->get('translator')->trans('Fecha de Publicación').': <span>'.$foro->getFechaPublicacion()->format('d/m/Y').'</span></span>
+                    </div>';
+        if ($resp_ft)
+        {
+            $html .= '<div class="col-auto">
+                        <span class="resp_ft"><i class="material-icons ic-msg">message</i> '.$resp_ft.'</span>
+                    </div>';
+        }
+        $html .= '</div>';
+
+        if (!$foro_id)
+        {
+            $html .= '</li>';
+        }
+
+        $return = array('html' => $html);
+
+        $return = json_encode($return);
+        return new Response($return, 200, array('Content-Type' => 'application/json'));
+
+    }
+
+    public function ajaxEditForoAction(Request $request)
+    {
+        
+        $session = new Session();
+        $em = $this->getDoctrine()->getManager();
+        
+        $foro_id = $request->query->get('foro_id');
+
+        $foro = $this->getDoctrine()->getRepository('LinkComunBundle:CertiForo')->find($foro_id);
+        
+        $return = array('tema' => $foro->getTema(),
+                        'fechaPublicacion' => $foro->getFechaPublicacion()->format('d/m/Y'),
+                        'fechaVencimiento' => $foro->getFechaVencimiento()->format('d/m/Y'),
+                        'fechaPublicacionF' => $foro->getFechaPublicacion()->format('Y-m-d'),
+                        'fechaVencimientoF' => $foro->getFechaVencimiento()->format('Y-m-d'),
+                        'mensaje' => $foro->getMensaje());
+
+        $return = json_encode($return);
+        return new Response($return, 200, array('Content-Type' => 'application/json'));
+        
     }
 
 }
--- web/js/front/espacio_colaborativo.js
+++ web/js/front/espacio_colaborativo.js
@@ -26,4 +26,85 @@ $(document).ready(function() {
     	$('#fechaPublicacion').datepicker('setEndDate', endDate);
 	});
 
+	observeTopic();
+
 });
+
+function observeTopic()
+{
+	$('.newTopic').click(function(){
+		var foro_id = $(this).attr('data');
+		$('#foro_id').val(foro_id);
+		$('#mensaje_content').val('');
+		if (foro_id != 0)
+		{
+			$.ajax({
+		        type: "GET",
+		        url: $('#url_edit').val(),
+		        async: true,
+		        data: { foro_id: foro_id },
+		        dataType: "json",
+		        success: function(data) {
+		            $('#tema').val(data.tema);
+		            $('#fechaPublicacion').val(data.fechaPublicacion);
+		            $('#fechaVencimiento').val(data.fechaVencimiento);
+		            CKEDITOR.instances.mensaje.setData(data.mensaje);
+		            var startDate = new Date(data.fechaPublicacionF);
+		            startDate.setDate(startDate.getDate() + 1);
+		            var endDate = new Date(data.fechaVencimientoF);
+		            endDate.setDate(endDate.getDate() + 1);
+		            $('#fechaVencimiento').datepicker('setStartDate', startDate);
+			    	$('#fechaPublicacion').datepicker('setEndDate', endDate);
+		            //clearTimeout( timerId );
+		        },
+		        error: function(){
+		            console.log('Error editando el espacio colaborativo'); // Hay que implementar los mensajes de error para el frontend
+		            $('#button-comment').show();
+		        }
+		    });
+		}
+		else {
+			$('.form-control').val('');
+			CKEDITOR.instances.mensaje.setData('');
+		}
+	});
+
+}
+
+function saveForo()
+{
+	$('label.mensaje-error').hide();
+    $('#publicar').hide();
+    var foro_id = $('#foro_id').val();
+    $.ajax({
+        type: "POST",
+        url: $('#form').attr('action'),
+        async: true,
+        data: $("#form").serialize(),
+        dataType: "json",
+        success: function(data) {
+            var none = $('#none_foros');
+            if (none.length)
+            {
+                none.remove();
+            }
+            if (foro_id != '0')
+            {
+                $( "#liForo-"+foro_id ).html( data.html );
+            }
+            else {
+            	$( "#ul-foros" ).prepend(data.html);
+            }
+            observeTopic();
+            $('#publicar').show();
+            $('.btn_close_modal').trigger( "click" );
+            $('#fechaPublicacion').datepicker('setEndDate', null);
+            $('#fechaVencimiento').datepicker('setEndDate', null);
+            //clearTimeout( timerId );
+        },
+        error: function(){
+            console.log('Error guardando el registro de espacio colaborativo'); // Hay que implementar los mensajes de error para el frontend
+            $('#button-comment').show();
+        }
+    });
+}
