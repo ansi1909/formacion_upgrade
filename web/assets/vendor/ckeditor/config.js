b2a1025d40cdedaa15cca12e3f299d4fa188f3e2
Jose Velasquez <josenriquev@gmail.com>
2018-05-02 16:20:54 -0400

    Customizando el ckeditor config con dos tipos de toolbars.

--- web/assets/vendor/ckeditor/config.js
+++ web/assets/vendor/ckeditor/config.js
@@ -4,7 +4,25 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.toolbarGroups = [
+	/*config.toolbarGroups = [
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		'/',
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];*/
+
+	config.toolbar_Full = [
         { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
@@ -22,7 +40,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	/*config.toolbar = 'MyToolbar';
+	/*config.toolbar = 'MyToolbar';*/
 
 	config.toolbar_MyToolbar =
 	[
@@ -33,7 +51,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
 		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
 		{ name: 'tools', items : [ 'Maximize','-','About' ] }
-	];*/
+	];
 
 	config.removeButtons = 'Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,BidiLtr,BidiRtl,Language,Anchor,Flash,PageBreak,Iframe,ShowBlocks,About';
     
