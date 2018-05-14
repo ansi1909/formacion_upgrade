e7f550d9c4833ab697b3eac251748cf695b4b6f5
JOSE TAYUPO <Jtayupo.linkgerencial@gmail.com>
2018-05-14 13:43:24 -0400

    Rutas para soporte

--- web/js/front/soporte_virtual.js
+++ web/js/front/soporte_virtual.js
@@ -34,13 +34,18 @@ $(document).ready(function()
 		      return error;
 	   }
 
+	   function enviarCorreo(campos)
+	   {
+
+	   }
+
 
     ///Metodos propios de JQuery
 		    $('#correo_soporte_btn_enviar').click(function()//detectar si se oprime el boton enviar
 		      { 
 		          
 		          
-		          var error=null;
+		          var errores={'mensaje':0,'asunto':0,'correo':0};
 		          var campos={
 		          	           'correo':{'valor':$('#correo_soporte').val(),'id':'correo_soporte','error_id':'error_correo','error_visible':false},
 		          	           'asunto':{'valor':$('#asuntoV').val(),'id':'asuntoV','error_id':'error_asunto','error_visible':false},
@@ -51,13 +56,18 @@ $(document).ready(function()
 		          ///Verificar que los campos esten llenos, verifica desde el 
 
 		          
-		          error=campoVacio(campos.mensaje);
-		          error=campoVacio(campos.asunto);
+		          errores.mensaje=campoVacio(campos.mensaje);
+		          errores.asunto=campoVacio(campos.asunto);
 		          if (campos.correo.valor!=undefined) //si el campo correo se muestra al usuario
 		          {
-		              error=campoVacio(campos.correo,true);
+		              errores.correo=campoVacio(campos.correo,true);
 		          }
-		          console.log(error);
+		          
+		          // if ((errores.mensaje+errores.asunto+errores.correo)==0) //si no existen errores se procede a enviar el correo
+		          // {
+		          	 
+		          // }
+		          
 
 		      });
 
