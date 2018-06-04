329fe45be49e159e2b745802a11633a3a35ce899
JOSE TAYUPO <Jtayupo.linkgerencial@gmail.com>
2018-06-04 13:45:55 -0400

    Paginacion de tutoriales

--- web/js/front/tutoriales.js
+++ web/js/front/tutoriales.js
@@ -1,24 +1,23 @@
 $(document).ready(function() 
 {
 
-    
     $('.descargaTutorial').click(function()
     {
     	location.href =  $(this).data('ruta');
       	
     });
 
-   //  $(".table-card").paginate({
-   //      perPage: 4,
-   //      autoScroll: false,
-   //      paginatePosition: ['bottom'],
-   //      useHashLocation: true,
-   //      onPageClick: function() {
-   //      	$('html, body').animate({
-			//     scrollTop: 0
-			// },2000);
-   //      }
-   //  });
+    $(".table-card").paginate({
+        perPage: 4,
+        autoScroll: false,
+        paginatePosition: ['bottom'],
+        useHashLocation: true,
+        onPageClick: function() {
+        	$('html, body').animate({
+			    scrollTop: 0
+			},2000);
+        }
+    });
 
 
 
