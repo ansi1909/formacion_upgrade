9c6720dd4e9ae5d5e8896d061a643fb1243bf335
Jose Velasquez <josenriquev@gmail.com>
2018-07-27 14:45:24 -0400

    Función de BD para el reporte de Avance en Programas.

--- web/js/admin/reportes/resumenRegistros.js
+++ web/js/admin/reportes/resumenRegistros.js
@@ -11,29 +11,6 @@ $(document).ready(function() {
     	$('#grafico').hide();
     });
 
-    $('#excel').click(function(){
-    	$('#excel').hide();
-    	$('#excel-loader').show();
-    	$.ajax({
-			type: "POST",
-			url: $('#form').attr('action'),
-			async: true,
-			data: $("#form").serialize()+'&excel=1',
-			dataType: "json",
-			success: function(data) {
-				$('#excel-loader').hide();
-	        	$("#excel-link").attr("href", data.archivo);
-	        	$('#excel-link').show();
-			},
-			error: function(){
-				$('#div-error-server').html($('#error-msg').val());
-				notify($('#div-error-server').html());
-				$('.descargable').hide();
-    			$('.generable').show();
-			}
-		});
-    });
-
     $('#pdf').click(function(){
     	$('#pdf').hide();
     	$('#pdf-loader').show();
