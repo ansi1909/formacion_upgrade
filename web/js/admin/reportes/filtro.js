eda6bc122f00f6401da540979f575d03c7db8c28
Jose Velasquez <josenriquev@gmail.com>
2018-08-03 11:08:18 -0400

    Ajustando el filtro.js para que traiga los programas si el usuario es de empresa.

--- web/js/admin/reportes/filtro.js
+++ web/js/admin/reportes/filtro.js
@@ -2,77 +2,42 @@ $(document).ready(function() {
 
 	var filtro_programas = $('#filtro_programas').val();
 	var filtro_tema = $('#filtro_tema').val();
+	var empresa_id = $('#empresa_id').val();
 	
 	if (filtro_programas == '1')
 	{
+
 		$('#empresa_id').change(function(){
 			$('#pagina_id').hide();
 			$('#pagina-loader').show();
 			$('#reporte').hide();
-			$.ajax({
-				type: "GET",
-				url: $("#url_programas").val(),
-				async: true,
-				data: { empresa_id: $("#empresa_id").val() },
-				dataType: "json",
-				success: function(data) {
-					$('#pagina_id').html(data.options);
-					$('#pagina_id').show();
-					$('#pagina-loader').hide();
-				},
-				error: function(){
-					$('#div-error-server').html($('#error-msg-paginas').val());
-					notify($('#div-error-server').html());
-				}
-			});
+			selectProgramas();
 		});
+
+		if (empresa_id != '')
+		{
+			selectProgramas();
+		}
+
 	}
 
 	if (filtro_tema == '1')
 	{
+
 		$('#empresa_id').change(function(){
 			$('#pagina_id').hide();
 			$('#pagina-loader').show();
 			$('#reporte').hide();
-			$.ajax({
-				type: "GET",
-				url: $("#url_programas").val(),
-				async: true,
-				data: { empresa_id: $("#empresa_id").val() },
-				dataType: "json",
-				success: function(data) {
-					$('#pagina_id').html(data.options);
-					$('#pagina_id').show();
-					$('#pagina-loader').hide();
-				},
-				error: function(){
-					$('#div-error-server').html($('#error-msg-paginas').val());
-					notify($('#div-error-server').html());
-				}
-			});
+			selectProgramas();
+			observePagina();
 		});
 
-		$('#pagina_id').change(function(){
-			$('#tema_id').hide();
-			$('#tema-loader').show();
-			$('#reporte').hide();
-			$.ajax({
-				type: "GET",
-				url: $("#url_temas").val(),
-				async: true,
-				data: { empresa_id: $("#empresa_id").val() , pagina_id: $("#pagina_id").val() },
-				dataType: "json",
-				success: function(data) {
-					$('#tema_id').html(data.options);
-					$('#tema_id').show();
-					$('#tema-loader').hide();
-				},
-				error: function(){
-					$('#div-error-server').html($('#error-msg-paginas').val());
-					notify($('#div-error-server').html());
-				}
-			});
-		});
+		if (empresa_id != '')
+		{
+			selectProgramas();
+			observePagina();
+		}
+
 	}
 
     $('.datePicker').datepicker({
@@ -130,3 +95,48 @@ $(document).ready(function() {
 	});
 
 });
+
+function selectProgramas()
+{
+	$.ajax({
+		type: "GET",
+		url: $("#url_programas").val(),
+		async: true,
+		data: { empresa_id: $("#empresa_id").val() },
+		dataType: "json",
+		success: function(data) {
+			$('#pagina_id').html(data.options);
+			$('#pagina_id').show();
+			$('#pagina-loader').hide();
+		},
+		error: function(){
+			$('#div-error-server').html($('#error-msg-paginas').val());
+			notify($('#div-error-server').html());
+		}
+	});
+}
+
+function observePagina()
+{
+	$('#pagina_id').change(function(){
+		$('#tema_id').hide();
+		$('#tema-loader').show();
+		$('#reporte').hide();
+		$.ajax({
+			type: "GET",
+			url: $("#url_temas").val(),
+			async: true,
+			data: { empresa_id: $("#empresa_id").val() , pagina_id: $("#pagina_id").val() },
+			dataType: "json",
+			success: function(data) {
+				$('#tema_id').html(data.options);
+				$('#tema_id').show();
+				$('#tema-loader').hide();
+			},
+			error: function(){
+				$('#div-error-server').html($('#error-msg-paginas').val());
+				notify($('#div-error-server').html());
+			}
+		});
+	});
+}
