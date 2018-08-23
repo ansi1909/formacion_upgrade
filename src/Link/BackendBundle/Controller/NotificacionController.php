23bc16a3b3a744a6c3c63da1ff33b4c76f166ef2
José Velásquez <josenriquev@gmail.com>
2018-08-23 19:48:33 -0400

    Restructurando el módulo de avisos programados

--- src/Link/BackendBundle/Controller/NotificacionController.php
+++ src/Link/BackendBundle/Controller/NotificacionController.php
@@ -12,6 +12,7 @@ use Link\ComunBundle\Entity\AdminTipoNotificacion;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Yaml\Yaml;
 
 class NotificacionController extends Controller
 {
@@ -40,7 +41,7 @@ class NotificacionController extends Controller
         $empresas = array();
         $notificaciones = array();
         
-        $usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($session->get('usuario')['id']); 
+        $usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($session->get('usuario')['id']);
 
         if ($usuario->getEmpresa()) 
         {
@@ -227,6 +228,151 @@ class NotificacionController extends Controller
         return $this->render('LinkBackendBundle:Notificacion:edit.html.twig', array('form' => $form->createView(),
                                                                                     'notificacion' => $notificacion,
                                                                                     'usuario' => $usuario));
+        
+    }
+
+    public function programadosAction($app_id)
+    {
+
+        $session = new Session();
+        $f = $this->get('funciones');
+        
+        if (!$session->get('ini') || $f->sesionBloqueda($session->get('sesion_id')))
+        {
+            return $this->redirectToRoute('_loginAdmin');
+        }
+        else {
+
+            $session->set('app_id', $app_id);
+            if (!$f->accesoRoles($session->get('usuario')['roles'], $session->get('app_id')))
+            {
+                return $this->redirectToRoute('_authException');
+            }
+        }
+        $f->setRequest($session->get('sesion_id'));
+
+        $em = $this->getDoctrine()->getManager();
+
+        $usuario = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($session->get('usuario')['id']);
+
+        $notificaciones = array();
+        
+        if ($usuario->getEmpresa()) 
+        {
+            $notificacionesdb = $this->getDoctrine()->getRepository('LinkComunBundle:AdminNotificacion')->findByEmpresa($usuario->getEmpresa());
+        }
+        else {
+            $query = $em->createQuery("SELECT n FROM LinkComunBundle:AdminNotificacion n
+                                       JOIN n.empresa e 
+                                       WHERE e.activo = :activo 
+                                       ORDER BY n.id ASC")
+                         ->setParameter('activo', true);
+            $notificacionesdb = $query->getResult();
+        }
+
+        foreach ($notificacionesdb as $notificacion)
+        {
+
+            $query = $em->createQuery('SELECT COUNT(np.id) FROM LinkComunBundle:AdminNotificacionProgramada np 
+                                        WHERE np.notificacion = :notificacion_id')
+                        ->setParameter('notificacion_id', $notificacion->getId());
+            $tiene_programados = $query->getSingleScalarResult();
+
+            $notificaciones[] = array('id' => $notificacion->getId(),
+                                      'asunto' => $notificacion->getAsunto(),
+                                      'empresa' => $notificacion->getEmpresa()->getNombre(),
+                                      'tipo' => $notificacion->getTipoNotificacion()->getNombre(),
+                                      'tiene_programados' => $tiene_programados);
+        }
+
+        return $this->render('LinkBackendBundle:Notificacion:programados.html.twig', array('notificaciones' => $notificaciones,
+                                                                                           'usuario' => $usuario));
+
+    }
+
+    public function ajaxProgramadosAction(Request $request)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $f = $this->get('funciones');
+        $notificacion_id = $request->query->get('notificacion_id');
+
+        $notificacion = $this->getDoctrine()->getRepository('LinkComunBundle:AdminNotificacion')->find($notificacion_id);
+        
+        $query = $em->createQuery("SELECT np FROM LinkComunBundle:AdminNotificacionProgramada np 
+                                    WHERE np.notificacion = :notificacion_id AND np.grupo IS NULL 
+                                    ORDER BY np.fechaDifusion ASC")
+                    ->setParameter('notificacion_id', $notificacion_id);
+        $nps = $query->getResult();
+
+        $html = $this->renderView('LinkBackendBundle:Notificacion:notificacionesProgramadas.html.twig', array('nps' => $nps));
+
+        $return = array('html' => $html,
+                        'notificacion' => $notificacion->getAsunto());
+
+        $return = json_encode($return);
+        return new Response($return, 200, array('Content-Type' => 'application/json'));
+        
+    }
+
+    public function ajaxTreeGrupoProgramadoAction($notificacion_programada_id, Request $request)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $f = $this->get('funciones');
+        $yml = Yaml::parse(file_get_contents($this->get('kernel')->getRootDir().'/config/parametros.yml'));
+
+        $notificacion_programada = $this->getDoctrine()->getRepository('LinkComunBundle:AdminNotificacionProgramada')->find($notificacion_programada_id);
+
+        $return = array();
+        switch ($notificacion_programada->getTipoDestino()->getId())
+        {
+            case $yml['parameters']['tipo_destino']['todos']:
+                $return[] = array('text' => 'N/A',
+                                  'state' => array('opened' => true),
+                                  'icon' => 'fa fa-angle-double-right');
+                break;
+            case $yml['parameters']['tipo_destino']['nivel']:
+                $entidad = $this->getDoctrine()->getRepository('LinkComunBundle:AdminNivel')->find($notificacion_programada->getEntidadId());
+                $return[] = array('text' => $entidad->getNombre(),
+                                  'state' => array('opened' => true),
+                                  'icon' => 'fa fa-angle-double-right');
+                break;
+            case $yml['parameters']['tipo_destino']['programa']:
+                $entidad = $this->getDoctrine()->getRepository('LinkComunBundle:CertiPagina')->find($notificacion_programada->getEntidadId());
+                $return[] = array('text' => $entidad->getNombre(),
+                                  'state' => array('opened' => true),
+                                  'icon' => 'fa fa-angle-double-right');
+                break;
+            case $yml['parameters']['tipo_destino']['grupo']:
+                $nps = $this->getDoctrine()->getRepository('LinkComunBundle:AdminNotificacionProgramada')->findByGrupo($notificacion_programada->getId());
+                foreach ($nps as $np)
+                {
+                    $participante = $this->getDoctrine()->getRepository('LinkComunBundle:AdminUsuario')->find($np->getEntidadId());
+                    $return[] = array('text' => $participante->getNombre().' '.$participante->getApellido(),
+                                      'state' => array('opened' => true),
+                                      'icon' => 'fa fa-angle-double-right');
+                }
+                break;
+            case $yml['parameters']['tipo_destino']['no_ingresado']:
+                $return[] = array('text' => 'N/A',
+                                  'state' => array('opened' => true),
+                                  'icon' => 'fa fa-angle-double-right');
+                break;
+            case $yml['parameters']['tipo_destino']['no_ingresado_programa']:
+                $return[] = array('text' => 'N/A',
+                                  'state' => array('opened' => true),
+                                  'icon' => 'fa fa-angle-double-right');
+                break;
+            case $yml['parameters']['tipo_destino']['aprobados']:
+                $return[] = array('text' => 'N/A',
+                                  'state' => array('opened' => true),
+                                  'icon' => 'fa fa-angle-double-right');
+                break;
+        }
+
+        $return = json_encode($return);
+        return new Response($return, 200, array('Content-Type' => 'application/json'));
         
     }
 
