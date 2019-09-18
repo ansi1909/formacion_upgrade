960548b69d1db6d40d0716170a20174ed31ee9f8
mugiwarayial <yasaacisaias@gmail.com>
2019-09-18 10:22:55 -0400

    ajustes en noticias y novedades. biblioteca virtual. backend

--- web/js/admin/biblioteca.js
+++ web/js/admin/biblioteca.js
@@ -5,13 +5,6 @@ $(document).ready(function() {
 	
 	observe();
 
-    $('.iframe-btn').fancybox({	
-		'width'		: 900,
-		'height'	: 900,
-		'type'		: 'iframe',
-        'autoScale' : false,
-		'autoSize'	: false
-    });
 
     $('#tipo_biblioteca_id').change(function(){
         observe();
@@ -53,9 +46,54 @@ $(document).ready(function() {
 
 	$("form :input").attr("autocomplete", "off");
         
-    $("#btn_clear_img").on("click",function(event) {
+    $('.uploadFileHref').click(function(){
+		$('#file_input').val($(this).attr('data-etiqueta'));
+		$('#div-error').hide();
+	});
+
+	$('.fileupload').fileupload({
+        url: $('#url_upload').val(),
+        dataType: 'json',
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|pdf)$/i,
+        add: function (e, data) {
+            var goUpload = true;
+            var uploadFile = data.files[0];
+            var file_input = $('#file_input').val();
+            if (!(/\.(gif|jpg|jpeg|tiff|png)$/i).test(uploadFile.name) && file_input == 'imagen') {
+                $('#div-error ul').html("<li>- Debes seleccionar sólo archivo de imagen</li>");
+                goUpload = false;
+            }
+            if (!(/\.(pdf)$/i).test(uploadFile.name) && file_input == 'pdf') {
+                $('#div-error ul').html("<li>- Debes seleccionar sólo archivo PDF</li>");
+                goUpload = false;
+            }
+            if (goUpload == true) {
+                data.submit();
+            }
+            else {
+                $('#div-error ul').show();
+                notify($('#div-error').html());
+            }
+        },
+        done: function (e, data) {
+            $.each(data.result.response.files, function (index, file) {
+                var file_input = $('#file_input').val();
+                var uploads = $('#uploads').val();
+				var base_upload = $('#base_upload').val();
+				console.log(base_upload+'aqui');
+                if (file_input == 'imagen')
+                {
+                    var img = $('#foto_img');
+                    img.attr("src", uploads+base_upload+file.name);
+                }
+                $('#'+file_input).val(base_upload+file.name);
+            });
+        }
+    });
+
+    $("#btn_clear").on("click",function(event) {
         $("#imagen").val("");
-        $("#figure").html('<img src="'+$('#photo').val()+'">');
+        $("#figure").html('<img id="foto_img" src="'+$('#photo').val()+'" style="width: 512px; height: auto; margin: 0 1rem;">');
     });
 
     $("#btn_clear_video").on("click",function(event) {
--- web/js/admin/novedad.js
+++ web/js/admin/novedad.js
@@ -12,14 +12,7 @@ $(document).ready(function() {
 	    clearBtn: true
 	}); 
 
-    $('.iframe-btn').fancybox({	
-		'width'		: 900,
-		'height'	: 900,
-		'type'		: 'iframe',
-        'autoScale' : false,
-		'autoSize'	: false
-    });
-
+    
     CKEDITOR.replace( 'resumen', {
 		filebrowserBrowseUrl : root_site+'/jq/ResponsiveFilemanager/filemanager/dialog.php?type=2&editor=ckeditor&rootFolder=recursos/noticias'+usuario_empresa,
 		filebrowserUploadUrl : root_site+'/jq/ResponsiveFilemanager/filemanager/dialog.php?type=2&editor=ckeditor&rootFolder=recursos/noticias'+usuario_empresa,
@@ -68,9 +61,54 @@ $(document).ready(function() {
 		deslen2.value = parseInt(editor_contenido.replace(/<[^>]+>/g, '').length);
 	});
 
-	$("#btn_clear").on("click",function(event) {
+	$('.uploadFileHref').click(function(){
+		$('#file_input').val($(this).attr('data-etiqueta'));
+		$('#div-error').hide();
+	});
+
+	$('.fileupload').fileupload({
+        url: $('#url_upload').val(),
+        dataType: 'json',
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|pdf)$/i,
+        add: function (e, data) {
+            var goUpload = true;
+            var uploadFile = data.files[0];
+            var file_input = $('#file_input').val();
+            if (!(/\.(gif|jpg|jpeg|tiff|png)$/i).test(uploadFile.name) && file_input == 'imagen') {
+                $('#div-error ul').html("<li>- Debes seleccionar sólo archivo de imagen</li>");
+                goUpload = false;
+            }
+            if (!(/\.(pdf)$/i).test(uploadFile.name) && file_input == 'pdf') {
+                $('#div-error ul').html("<li>- Debes seleccionar sólo archivo PDF</li>");
+                goUpload = false;
+            }
+            if (goUpload == true) {
+                data.submit();
+            }
+            else {
+                $('#div-error ul').show();
+                notify($('#div-error').html());
+            }
+        },
+        done: function (e, data) {
+            $.each(data.result.response.files, function (index, file) {
+                var file_input = $('#file_input').val();
+                var uploads = $('#uploads').val();
+				var base_upload = $('#base_upload').val();
+				console.log(base_upload+'aqui');
+                if (file_input == 'imagen')
+                {
+                    var img = $('#foto_img');
+                    img.attr("src", uploads+base_upload+file.name);
+                }
+                $('#'+file_input).val(base_upload+file.name);
+            });
+        }
+    });
+
+    $("#btn_clear").on("click",function(event) {
         $("#imagen").val("");
-        $("#figure").html('<img src="'+$('#photo').val()+'">');
+        $("#figure").html('<img id="foto_img" src="'+$('#photo').val()+'" style="width: 512px; height: auto; margin: 0 1rem;">');
     });
 
     $("#btn_clear2").on("click",function(event) {
