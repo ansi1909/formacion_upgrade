c33d7a28a01609f70ee42c4bbf748c280191e7dc
JOSE TAYUPO <josetayupo@gmail.com>
2019-11-29 16:23:16 -0400

    [Verificacion de correo unico para cada participante : back - front]

--- src/Link/ComunBundle/Services/Functions.php
+++ src/Link/ComunBundle/Services/Functions.php
@@ -3601,4 +3601,19 @@ class Functions
 
     }
 
+    public function searchMail($mail,$empresa_id,$usuario_id=0){
+        //usuario_id = 0 cuando no se que quiere comprobar los datos de correo para un usuario en especifico
+        $em = $this->em;
+        $query = $em->createQuery('SELECT COUNT(u.id) FROM LinkComunBundle:AdminUsuario u 
+                                    WHERE u.correoCorporativo =:mail
+                                    AND (u.correoPersonal =:mail OR u.correoCorporativo =:mail)
+                                    AND u.empresa =:empresa_id 
+                                    AND u.id !=:usuario_id')
+                ->setParameters(['mail'=> $mail,'empresa_id'=>$empresa_id,'usuario_id'=>$usuario_id]);
+        $result = $query->getSingleScalarResult();
+
+        return $result;
+    }
+
+
 }
