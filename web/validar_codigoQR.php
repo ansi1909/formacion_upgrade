cba8777b29ab23e310041839a534c10ceda6d9a1
Jose Tayupo <josetayupo@gmail.com>
2020-03-31 09:33:13 -0400

    ajustes qr

--- web/validar_codigoQR.php
+++ web/validar_codigoQR.php
@@ -132,10 +132,10 @@ else {
 ?>
 	<!DOCTYPE html>
 	<html lang="es">
-	    <head><meta charset="gb18030">
-	        
+	    <head>
+	        <meta charset="UTF-8">
 	        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	        <meta http-equiv="Content-type"  content="text/html">
 	        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700|Open+Sans:300,400,400i,600,600i,700|Roboto:300,400,500,700" rel="stylesheet">
 	        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	        <link rel="stylesheet" href="<?php echo $url_web ?>front/base_styles/css/bootstrap/bootstrap.css">
