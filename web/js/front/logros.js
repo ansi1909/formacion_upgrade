d1c5d0ae05ed106e28ff9e8cf6639a4e46445a9a
Domingo Cabeza <domingoacd1@gmail.com>
2021-04-20 10:36:01 -0400

    Agregando funcionalidad para ver los logros de un usuario en un programa

---
+++ web/js/front/logros.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.j-btn-achievement').each(function() {
+        const achievementId = this.dataset.id;
+        console.log(achievementId);
+
+        if (achievementId) {
+            
+        }
+    });
+});
