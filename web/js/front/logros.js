1cd01821e69eb9bcc535dd7921e583465fcce2b7
Domingo Cabeza <domingoacd1@gmail.com>
2021-04-21 15:51:46 -0400

    Agregados avisos de las notificaciones

--- web/js/front/logros.js
+++ web/js/front/logros.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
             $('.ranking-loader').addClass('d-flex');
             setTimeout(() => {
                 $('.ranking-loader').removeClass('d-flex');
-                $('#achievements-container').addClass('show');
                 $('#study_plan').removeClass('show');
+                $('#achievements-container').addClass('show');
+                $("#achievements-container").animate({ scrollTop: 0 }, "fast");
             }, 1000);
         }
     });
@@ -16,5 +17,9 @@ $(document).ready(function() {
     $('.j-btn-back-to-plan').click(function(e) {
         $('#achievements-container').removeClass('show');
         $('#study_plan').addClass('show');
+    });
+
+    $('#modal-ranking-big-notification .btn_close_modal').click(function(e) {
+        $('#modal-ranking-big-notification').removeClass('show');
     });
 });
