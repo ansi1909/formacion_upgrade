708302287a46a51f77a53c66dda591b29fae7626
Jose Tayupo <josetayupo@gmail.com>
2021-08-11 15:33:26 -0400

    notificaciones push

--- web/js/front/alarmas.js
+++ web/js/front/alarmas.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 
     getAlarma();
     getNotificaciones();
+    removeNotificacionesPush();
+
+
 
     $('#comentario').focus(function(){
         $('.error').hide();
@@ -52,12 +55,17 @@ function getAlarma()
         async: true,
         dataType: "json",
         success: function(data) {
-            const alertNotificacion = document.getElementById("alert_notificacion");
+            //const alertNotificacion = document.getElementById("alert_notificacion");
             $('#noti').html(data.html);
             if (data.sonar == 1) {
                 $('#sonar').show();
-                alertNotificacion.classList.add("show");
-                
+
+                //notificaciones push
+                if (data.push.length){
+                    console.log(data.push.length);
+                   crearDivAlert(data.push);
+                }
+
          
             }
             else{
@@ -118,6 +126,7 @@ function observeMuro()
 
 function getLeido(noti_id)
 {
+    console.log('Marcando como leido');
     $.ajax({
         type: "POST",
         url: $('#url_leido').val(),
@@ -139,9 +148,20 @@ function getNotificaciones()
 {
     timer = setInterval(function(){
         getAlarma();
-    }, 60000);
+    }, 45000);
    
 }
+
+function removeNotificacionesPush(){
+    timer = setInterval(function(){
+        removeNotificationBox();
+    }, 20000);
+}
+
+function removeNotificationBox(){
+    $("div").remove(".notification-box");
+}
+
 
 function observeLike()
 {
@@ -168,4 +188,70 @@ function observeLike()
             }
         });
     });
+}
+
+function crearDivAlert(notificaciones){
+    const container   = document.getElementById("alert-notif-container");
+    for(const index in notificaciones){
+            console.log(notificaciones[index]);
+            var divAlert    = document.createElement("div");
+            var spanIcon    = document.createElement("span");
+            var icon        = document.createElement("i");
+            var spanDescrip = document.createElement("span");
+            var buttonClose = document.createElement("button");
+            var spanButton  = document.createElement("span");
+            var link        = document.createElement("a"); 
+            var input      = document.createElement("input");
+
+            input.setAttribute("type","hidden");
+            
+
+            divAlert.classList.add("alert", "alert-success","alert-dismissible", "fade","notification-box", "notification-ranking","pl-3", "py-1","position-relative" ,"show");
+            divAlert.setAttribute("role","alert");
+            
+
+            icon.classList.add("material-icons","icNotify");
+            icon.innerHTML = notificaciones[index].icono;
+            
+            spanIcon.classList.add("stickerNotify",notificaciones[index].css);
+            link.classList.add("link-alert-notif");
+            if (!notificaciones[index].muro){
+                link.setAttribute("href",notificaciones[index].href);
+               
+            }else{
+                link.setAttribute("href","#");
+                link.setAttribute("data-toggle","modal");
+                link.setAttribute("data-target","#modalMn");
+                link.classList.add("click");
+            }
+            
+            link.classList.add("leido");
+            link.setAttribute("data",notificaciones[index].id);
+            link.setAttribute("titulo",notificaciones[index].titulo);
+            
+            spanDescrip.classList.add("notification-ranking__text");
+            spanDescrip.innerHTML = notificaciones[index].descripcion;
+
+            buttonClose.classList.add("close");
+            buttonClose.setAttribute("type","button");
+            buttonClose.setAttribute("data-dismiss","alert");
+            buttonClose.setAttribute("aria-label","Close");
+            buttonClose.classList.add("pl-1");
+
+            spanButton.setAttribute("aria-hidden",true);
+            spanButton.innerHTML = "&times;";
+            
+            link.appendChild(spanDescrip);
+            buttonClose.appendChild(spanButton);
+            spanIcon.appendChild(icon);
+            divAlert.appendChild(spanIcon);
+            divAlert.appendChild(link);
+            divAlert.appendChild(buttonClose);
+            container.appendChild(divAlert);
+    }
+
+    $('.link-alert-notif').click(function(){
+        removeNotificationBox();
+    });
+
 }
